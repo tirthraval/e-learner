86ca4ae45a2802d79e62d07db7be4b8bf0f08836
Tirth Raval <40597175+tirthraval@users.noreply.github.com>
2022-11-10 12:39:36 -0600

    Add expert componants

--- src/compontants/Experts.js
+++ src/compontants/Experts.js
@@ -1,10 +1,13 @@
 import Header from "./Header";
+import Expert from "./Expert";
 
 const Experts = () =>{
     return (
         <>
+            <Header/>
             <div>
-                
+                <Expert src ="./img/gamer.png" name = "" lang =" "/>
+                <Expert src = "./img/woman.png" name = " " lang = ""/>
             </div>
         </>
 
